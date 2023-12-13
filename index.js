e72a3d4c883c7a173c21d30d325d23a20b6afd06
Mary Watiri <watirimary30@gmail.com>
2023-12-13 15:37:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-
-// Write your code here
+const[num1,num2,num3,num4]=[2,31,19,5];
+const arr = [1,10,11,9,18,20,12,22];
+let multiply = num1*num2;
+let random = Math.floor(Math.random()*10)+1;
+let mod = num3%num4;
+let max = Math.max (14,12,8,9,20);
